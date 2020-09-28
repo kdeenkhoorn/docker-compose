f762a797f8c000b4bf7c1761d40b93b61c7415e1
root <root@int.kdedesign.nl>
2020-09-28 19:00:30 +0200

    Changed working folder to /opt/docker

--- docker-compose-restart.sh
+++ docker-compose-restart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # restart services in docker-compose
-docker-compose -p int -f /home/klaas/Development/docker/myrc/docker-compose.yaml stop $@
-docker-compose -p int -f /home/klaas/Development/docker/myrc/docker-compose.yaml rm -f -v $@
-docker-compose -p int -f /home/klaas/Development/docker/myrc/docker-compose.yaml up --no-start $@
-docker-compose -p int -f /home/klaas/Development/docker/myrc/docker-compose.yaml start $@
+docker-compose -p int -f $PWD/docker-compose.yaml stop $@
+docker-compose -p int -f $PWD/docker-compose.yaml rm -f -v $@
+docker-compose -p int -f $PWD/docker-compose.yaml up --no-start $@
+docker-compose -p int -f $PWD/docker-compose.yaml start $@
 
--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose -p int -f /home/klaas/Development/docker/myrc/docker-compose.yaml down
+docker-compose -p int -f $PWD/docker-compose.yaml down
